8108b5f389a8189fb368fa7fa31b32e18a191637
Julio Cesar <julio.souzam@gmail.com>
2019-12-26 23:15:52 -0300

    start setup file - step 2

--- setup.py
+++ setup.py
@@ -1,23 +1,48 @@
 from subprocess import run
+import crypt
 
 USER = input('Qual usuário? ') or 'julio'
 HOSTNAME = input('Qual o hostname? ') or 'juliocesar'
-PASSWORD = input('Qual a senha? (default: admin)') or 'admin'
+PASSWORD = crypt.crypt(
+    input('Qual a senha do usuário {USER}? (default: admin)') or 'admin')
 LANG = input('Qual a lang? (default: pt_BR) ') or 'pt_BR'
 ENCODE = input('Qual o encode? (default: UTF-8)') or 'UTF-8'
 TIME_ZONE = input(
     'Qual o timezone? (default: America/Fortaleza) ') or 'America/Fortaleza'
 
+exit
 try:
     run(['sed', '-i', '/{LANG}/,+1 s/^#//', '/etc/locale.gen'], check=True)
     run(['locale-gen'], check=True)
     run(['echo', 'LANG={LANG}.{ENCODE}', '>', '/etc/locale.conf'], check=True)
-    run(['export', 'LANG={LANG}.{ENCODE}'], check=True)
 
     run(['ln', '-sf', '/usr/share/zoneinfo/' +
          TIME_ZONE, '/etc/localtime'], check=True)
 
     run(['sed', '-i', '/multilib\\]/,+1 s/^#//', '/etc/pacman.conf'], check=True)
     run(['pacman', '-Sy'], check=True)
+
+    run(['echo {HOSTNAME}', '>', '/etc/hostname'])
+
+    run(['echo', '127.0.0.1\tlocalhost', '>', '/etc/hosts'], check=True)
+
+    run(['sed', '-i', 's/nameserver/#nameserver/g', '/etc/resolv.conf'], check=True)
+    run(['echo', 'nameserver\t8.8.8.8', '>>', '/etc/resolv.conf'], check=True)
+    run(['echo', 'nameserver\t8.8.4.4', '>>', '/etc/resolv.conf'], check=True)
+
+    run(['pacman', '-S', 'wpa_supplicant', 'dialog',
+         'iw', 'networkmanager', 'sudo'], check=True)
+
+    run(['systemctl', 'enable', 'NetworkManager'], check=True)
+    run(['useradd', '-m', '-g', 'users', '-G', 'log,sys,wheel,rfkill,dbus',
+         '-s', '/bin/bash', '-p', PASSWORD, USER], check=True)
+
+    print('#### DEFINIR SENHA ROOT #####')
+    run(['passwd'], check=True)
+
+    run(['pacman', '-S', 'bash-completion'], check=True)
+
+    run(['sed', '-i', '/%wheel ALL=(ALL) ALL/s/^#//', '/etc/sudoers'], check=True)
+
 except Exception as ex:
     print(ex)
