a3f131ff60de9b7b295dd5ec36ad288d46103e58
Julio Cesar <julio.souzam@gmail.com>
2019-12-26 23:19:16 -0300

    remove variables inside string

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ import crypt
 USER = input('Qual usuário? ') or 'julio'
 HOSTNAME = input('Qual o hostname? ') or 'juliocesar'
 PASSWORD = crypt.crypt(
-    input('Qual a senha do usuário {USER}? (default: admin)') or 'admin')
+    input('Qual a senha do usuário ' + USER + '? (default: admin)') or 'admin')
 LANG = input('Qual a lang? (default: pt_BR) ') or 'pt_BR'
 ENCODE = input('Qual o encode? (default: UTF-8)') or 'UTF-8'
 TIME_ZONE = input(
     'Qual o timezone? (default: America/Fortaleza) ') or 'America/Fortaleza'
-
 exit
 try:
-    run(['sed', '-i', '/{LANG}/,+1 s/^#//', '/etc/locale.gen'], check=True)
+    run(['sed', '-i', '/' + LANG + '/,+1 s/^#//', '/etc/locale.gen'], check=True)
     run(['locale-gen'], check=True)
-    run(['echo', 'LANG={LANG}.{ENCODE}', '>', '/etc/locale.conf'], check=True)
+    run(['echo', 'LANG=' + LANG + '.' + ENCODE + '', '>', '/etc/locale.conf'], check=True)
 
     run(['ln', '-sf', '/usr/share/zoneinfo/' +
          TIME_ZONE, '/etc/localtime'], check=True)
@@ -22,7 +21,7 @@ try:
     run(['sed', '-i', '/multilib\\]/,+1 s/^#//', '/etc/pacman.conf'], check=True)
     run(['pacman', '-Sy'], check=True)
 
-    run(['echo {HOSTNAME}', '>', '/etc/hostname'])
+    run(['echo ' + HOSTNAME, '>', '/etc/hostname'])
 
     run(['echo', '127.0.0.1\tlocalhost', '>', '/etc/hosts'], check=True)
 
